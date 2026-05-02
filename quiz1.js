9f762143d5f45645d041b32c5563b3d102064b8a
Eden10222 <classic1022@gmail.com>
2026-05-02 18:03:00 +0900

    Update quiz1.js

--- quiz1.js
+++ quiz1.js
@@ -1,17 +1,20 @@
 let current = 0;
 
-let score = {
-  A: 0,
-  B: 0
-};
+let score = { A: 0, B: 0 };
 
 const questions = [
-  { q: "주말에 뭐해?" },
-  { q: "친구가 부르면?" }
+  "나는 혼자가 편하다",
+  "나는 계획형이다",
+  "나는 감정보다 이성을 따른다",
+  "나는 새로운 걸 좋아한다",
+  "나는 사람 만나는 걸 좋아한다",
+  "나는 즉흥적이다",
+  "나는 감정 표현이 많다",
+  "나는 안정이 중요하다"
 ];
 
 function showQuestion() {
-  document.getElementById("question").innerText = questions[current].q;
+  document.getElementById("question").innerText = questions[current];
 }
 
 function answer(type) {
